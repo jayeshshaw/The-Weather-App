ddd3ef8cb0441312af08cb41814c720794fcc92a
Jayesh Shaw <jayesh.shaw10@gmail.com>
2022-04-28 18:09:38 +0530

    updated skeleton

--- script.js
+++ script.js
@@ -23,17 +23,16 @@ let weather = {
         const { speed } = data.wind;
 
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
-        
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+
 
 
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage =
-            "url('https://source.unsplash.com/1600x900/?" + name +"')";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
